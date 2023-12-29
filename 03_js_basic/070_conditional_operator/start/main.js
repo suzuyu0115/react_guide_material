09150faacd8c52f04772d65e49b5165157dafe0a
suzuyu0115 <szkyut0115@gmail.com>
2023-12-29 17:26:03 +0900

    first commit

---
+++ 03_js_basic/070_conditional_operator/start/main.js
@@ -0,0 +1,17 @@
+// 三項演算子（ ? : ）
+
+const a = 0;
+let resultA = a ? 10 : -10;
+
+// if(a) {
+//   resultA = "true";
+// } else {
+//   resultA = "false";
+// }
+console.log(resultA);
+
+function getResult() {
+  return a ? "true" : "false";
+}
+
+console.log(getResult());
---
+++ 03_js_basic/100_async_await/start/main.js
@@ -0,0 +1,19 @@
+// 非同期処理（async/await）
+let a = 0;
+
+init();
+async function init() {
+    try {
+        const result = await new Promise((resolve, reject) => {
+            setTimeout(() => {
+                a = 1;
+                reject(a)
+            }, 2000);
+        })
+            console.log(result);
+    } catch(e) {
+        console.log('catchが実行', e)
+    }
+    // }).catch((c) => {
+    // })
+}
