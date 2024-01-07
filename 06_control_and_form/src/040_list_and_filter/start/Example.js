6738598f5120f3c5828629bf9470903e3a3d02b8
suzuyu0115 <szkyut0115@gmail.com>
2024-01-07 17:50:51 +0900

    040_list_and_filter

--- 06_control_and_form/src/040_list_and_filter/start/Example.js
+++ 06_control_and_form/src/040_list_and_filter/start/Example.js
@@ -3,13 +3,22 @@ import { useState } from "react";
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
+  const [filterVal, setFilterVal] = useState("");
+
   return (
     <>
       <h3>配列のフィルター</h3>
+      <input type="text" value={filterVal}
+                        onChange={(e) => setFilterVal(e.target.value)} />
       <ul>
         {animals
+          .filter(animal => {
+            const isMatch = animal.indexOf(filterVal) !== -1;
+            console.log(animal.indexOf(filterVal))
+            return isMatch
+          })
           .map((animal) => (
-          <li>{animal}</li>
+          <li key={animal}>{animal}</li>
         ))}
       </ul>
     </>
