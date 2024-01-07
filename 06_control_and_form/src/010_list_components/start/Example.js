374ceac9c494c40dfd76233e9962ff56b0ade6f1
suzuyu0115 <szkyut0115@gmail.com>
2024-01-07 16:35:12 +0900

    010_list_components

--- 06_control_and_form/src/010_list_components/start/Example.js
+++ 06_control_and_form/src/010_list_components/start/Example.js
@@ -3,11 +3,22 @@ const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
 
+  const animalList = [];
+  for(const animal of animals) {
+    animalList.push(<li>{animal}</li>);
+  }
+  const helloAnimals = animals.map((animal) => <li>Hello, {animal}</li>)
+
   return (
     <>
       <h3>配列の操作</h3>
       <ul>
-        
+        {/* <li>{animals[0]}</li>
+        <li>{animals[1]}</li>
+        <li>{animals[2]}</li> */}
+        {/* {animalList} */}
+        {/* {helloAnimals} */}
+        {animals.map((animal) => <li>Hello, {animal}</li>)}
       </ul>
     </>
   );
