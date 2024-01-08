4487710fe6bbcb9c07830e0385a74b61372dc586
suzuyu0115 <szkyut0115@gmail.com>
2024-01-08 23:59:09 +0900

    Edit: 050_chakra_ui

--- 07_styling_component/src/050_chakra_ui/start/components/List.js
+++ 07_styling_component/src/050_chakra_ui/start/components/List.js
@@ -1,4 +1,5 @@
-import { StackDivider, VStack } from '@chakra-ui/react';
+import { StackDivider, VStack, HStack, IconButton, Text } from '@chakra-ui/react';
+import { VscCheck } from 'react-icons/vsc';
 
 const List = ({todos, deleteTodo}) => {
     const complete = (id) => {
@@ -15,10 +16,10 @@ const List = ({todos, deleteTodo}) => {
         >
             {todos.map(todo => {
                 return (
-                    <div key={todo.id}>
-                        <button onClick={() => complete(todo.id)}>完了</button>
-                        <span>{todo.content}</span>
-                    </div>
+                    <HStack key={todo.id} spacing="5">
+                        <IconButton onClick={() => complete(todo.id)} icon={<VscCheck />} isRound bgColor={"cyan.100"} opacity="0.5" >完了</IconButton>
+                        <Text>{todo.content}</Text>
+                    </HStack>
                 )
             })}
         </VStack>
