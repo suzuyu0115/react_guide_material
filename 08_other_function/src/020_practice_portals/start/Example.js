572bda504b99c5f5e95794118cb9cb186773b321
suzuyu0115 <szkyut0115@gmail.com>
2024-01-11 23:53:37 +0900

    020_practice_portals

--- 08_other_function/src/020_practice_portals/start/Example.js
+++ 08_other_function/src/020_practice_portals/start/Example.js
@@ -1,8 +1,15 @@
+import { createPortal } from "react-dom";
 import { useState } from "react";
+
 import Toast from "./components/Toast";
+
 
 const Example = () => {
   const [toastOpen, setToastOpen] = useState(false);
+  const ToastPortal = ({ children }) => {
+    const target = document.querySelector('.container.start')
+    return createPortal(children, target)
+  }
 
   return (
     <div>
@@ -12,7 +19,7 @@ const Example = () => {
       <p>ボタンを押すと.container.start要素にマウントされて、表示されるトーストを作成してください。トーストにはToastコンポーネントを使用してください。</p>
 
       <div className="container start"></div>
-      
+
       <button
         type="button"
         onClick={() => setToastOpen(true)}
@@ -21,10 +28,12 @@ const Example = () => {
         トーストを表示する
       </button>
       {toastOpen && (
+        <ToastPortal>
           <Toast
             visible={toastOpen}
             handleCloseClick={() => setToastOpen(false)}
           />
+        </ToastPortal>
       )}
     </div>
   );
