510b5456b998f1dfff93bc716ce5b2726419d218
suzuyu0115 <szkyut0115@gmail.com>
2024-01-28 16:08:41 +0900

    030_useContext

--- 11_hooks_p1/src/030_useContext/start/Example.js
+++ 11_hooks_p1/src/030_useContext/start/Example.js
@@ -1,5 +1,8 @@
 import Child from "./components/Child";
+import { createContext } from 'react';
 
+export const MyContext = createContext('hello');
+// useContext
 const Example = () => {
   const value = 'hello'
   return <Child value={value}/>;
