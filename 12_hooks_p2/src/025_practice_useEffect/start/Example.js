8f5900b8de0b94ffca85fd26601e97eadda9b796
suzuyu0115 <szkyut0115@gmail.com>
2024-02-11 14:48:01 +0900

    025_practice_useEffect

--- 12_hooks_p2/src/025_practice_useEffect/start/Example.js
+++ 12_hooks_p2/src/025_practice_useEffect/start/Example.js
@@ -1,7 +1,12 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Example = () => {
   const [checked, setChecked] = useState(false);
+
+  useEffect(() => {
+    checked && window.alert('checked!')
+  }, [checked]);
+
 
   return (
     <>
