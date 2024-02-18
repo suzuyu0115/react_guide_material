1327dcc360f273a008f66bff68dea941bdd818fc
suzuyu0115 <szkyut0115@gmail.com>
2024-02-19 00:07:41 +0900

    050_axios_get_state

--- 16_rest_api/src/040_axios_get_request/start/Example.js
+++ 16_rest_api/src/040_axios_get_request/start/Example.js
@@ -1,5 +1,14 @@
+import { useEffect } from 'react';
+import axios from 'axios';
+
 const Example = () => {
-  return <></>;
+  useEffect(() => {
+    const getUser = async () => {
+      const res = await axios.get('http://localhost:3003/user')
+      console.log(res.data)
+    }
+    getUser();
+  })
 };
 
 export default Example;
