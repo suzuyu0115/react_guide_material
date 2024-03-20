d5520b2f1c72155495e3bc18a8cb0a5c82e53857
suzuyu0115 <szkyut0115@gmail.com>
2024-03-20 17:20:22 +0900

    Add: useRouterの画面遷移

--- 17_nextjs_p1/start/src/pages/07_router/[name]/setting.js
+++ 17_nextjs_p1/start/src/pages/07_router/[name]/setting.js
@@ -1,3 +1,21 @@
-export default function Setting() {
-  return <h1>[name]/setting.js</h1>
+import { useRouter } from 'next/router'
+
+export default function Setting({ query }) {
+  const router = useRouter();
+  const clickHandler = () => {
+    router.reload()
+  }
+  return (
+    <>
+      <h1>routerから取得:{router.query.name}</h1>
+      <button onClick={clickHandler}>アクションによる画面遷移</button>
+    </>
+  )
+}
+
+export async function getServerSideProps({ query }) {
+
+  return {
+    props: { query }
+  }
 }
