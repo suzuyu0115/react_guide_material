d5e7e52f2bc6b72a13fa294f827fb7f5885a3a96
suzuyu0115 <szkyut0115@gmail.com>
2024-03-23 23:48:24 +0900

    Add: 09_multipage_state

--- 17_nextjs_p1/start/src/pages/09_multipage_state/index.js
+++ 17_nextjs_p1/start/src/pages/09_multipage_state/index.js
@@ -2,11 +2,16 @@
 import { useState } from "react";
 import Form from "../../components/form";
 import List from "../../components/list";
+import { useAppContext } from "../../context/AppContext";
+import { useRouter } from 'next/router';
+import Link from 'next/link'
 
 export default function PageLink() {
   const initialState = { name: "", birth: "" };
   const [value, setValue] = useState(initialState);
-  const [list, setList] = useState([]);
+  const [list, setList] = useAppContext();
+  const router = useRouter();
+
   const handleChange = (e) => {
     const inputId = e.target.id;
     const inputValue = e.target.value;
@@ -29,6 +34,14 @@ export default function PageLink() {
         birthValue={value.birth}
       />
       <List list={list} />
+      <button onClick={() => router.push('/09_multipage_state/list')}>
+        リストページへ
+      </button>
+      <Link href="/09_multipage_state/list">
+        <a>
+          アンカータグ
+        </a>
+      </Link>
     </div>
   );
 }
