55dd3d17a9c8dcb157a80ce54614bbfe539704f0
suzuyu0115 <szkyut0115@gmail.com>
2024-03-24 23:57:09 +0900

    getServerSideProps

--- 18_nextjs_p2/start/src/pages/010_SSR/index.js
+++ 18_nextjs_p2/start/src/pages/010_SSR/index.js
@@ -1,12 +1,28 @@
 import { useEffect, useState } from 'react'
 
-export default function SSR() {
+export default function SSR({ message }) {
   console.log('hello')
+  console.log(message)
   useEffect(() => {
     console.log('useEffect')
     window.localStorage.setItem('key', 'value')
+    document.cookie = 'val=0; path=/'
   }, [])
   const [state, setState] = useState('bye');
   const val = 0
   return <h3>{state}</h3>
+}
+
+export async function getServerSideProps(context) {
+
+  const { cookie } = context.req.headers;
+  console.log('cookie', cookie);
+
+  return {
+    // redirect: {
+    //   destination: '/',
+    //   parmanent: false
+    // },
+    props: { message: 'From Server Side Props' }
+  }
 }
