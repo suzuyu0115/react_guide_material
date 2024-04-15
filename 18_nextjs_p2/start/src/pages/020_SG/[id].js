252469ebe54cce511c643dfb1a50e57d48ef7124
suzuyu0115 <szkyut0115@gmail.com>
2024-04-15 20:02:47 +0900

    【SG】fallbackプロパティの挙動を理解しよう

--- 18_nextjs_p2/start/src/pages/020_SG/[id].js
+++ 18_nextjs_p2/start/src/pages/020_SG/[id].js
@@ -1,11 +1,17 @@
+import { useRouter } from 'next/router'
+
 export default function Page({ id }) {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <h3>Loading...</h3>
+  }
   return <h3>このページは{id}です。</h3>
 }
 
 export async function getStaticPaths() {
   return {
     paths: [{ params: { id: "1" } }, { params: { id: "2" } }],
-    fallback: false
+    fallback: true
   };
 }
 
