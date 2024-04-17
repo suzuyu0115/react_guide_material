f38f0b84a958d0433842ccbc6f3a83dd6d212574
suzuyu0115 <szkyut0115@gmail.com>
2024-04-17 23:55:19 +0900

    【おまけ】ISRを使ってみよう

--- 18_nextjs_p2/start/src/pages/020_SG/[id].js
+++ 18_nextjs_p2/start/src/pages/020_SG/[id].js
@@ -1,11 +1,11 @@
 import { useRouter } from 'next/router'
 
-export default function Page({ id }) {
+export default function Page({ id, date }) {
   const router = useRouter();
   if (router.isFallback) {
     return <h3>Loading...</h3>
   }
-  return <h3>このページは{id}です。</h3>
+  return <h3>このページは{id}です。{date}</h3>
 }
 
 export async function getStaticPaths() {
@@ -16,9 +16,12 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
+  const date = new Date;
   return {
     props: {
       id: params.id,
+      date: date.toJSON()
     },
+    // revalidate: 5
   };
 }
