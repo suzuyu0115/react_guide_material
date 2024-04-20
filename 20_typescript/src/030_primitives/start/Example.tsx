1d20ccdaf9da91eb27463aca1380b5124c1a4769
suzuyu0115 <szkyut0115@gmail.com>
2024-04-20 16:54:32 +0900

    030_primitives

--- 20_typescript/src/030_primitives/start/Example.tsx
+++ 20_typescript/src/030_primitives/start/Example.tsx
@@ -1,5 +1,25 @@
 const Example = () => {
   // 練習用
+  let str: string = 'Hello';
+  str = 'Bye';
+  console.log(str);
+
+  let num: number = 102;
+  console.log(num);
+
+  let bignum: bigint = 103n;
+  console.log(bignum);
+
+  let bool: boolean = true;
+  console.log(bool);
+
+  let nullish: null = null;
+  let undefindValue: undefined = undefined;
+
+
+  let trueVal: true = true;
+  let num123: 123 = 123;
+  let strHello: 'Hello' = 'Hello';
 };
 
 export default Example;
