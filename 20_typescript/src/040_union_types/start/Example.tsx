8c1c638475aa7d3a748f4a5fb46a749091a1c5c7
suzuyu0115 <szkyut0115@gmail.com>
2024-04-20 17:02:49 +0900

    040_union_types

--- 20_typescript/src/040_union_types/start/Example.tsx
+++ 20_typescript/src/040_union_types/start/Example.tsx
@@ -1,5 +1,23 @@
 const Example = () => {
   // 練習用
+  let strOrNum: string | number = 'Hello';
+  strOrNum = 123;
+  console.log(strOrNum);
+
+  let strOrNumOrBool: string | number | boolean = 'Hello';
+  strOrNumOrBool = true;
+  console.log(strOrNumOrBool);
+
+  let helloOrNumOrBool: 'Hello' | number | boolean = false;
+
+  type HelloOrNum = 'Hello' | number;
+  const hello: HelloOrNum = 'Hello';
+
+  type DayOfWeek =
+    | 'Monday'
+    | 'Tuesday'
+
+  const day: DayOfWeek = 'Monday';
 };
 
 export default Example;
