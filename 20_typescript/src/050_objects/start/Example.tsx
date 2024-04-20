15abf4f44c685583e58cd148ad7c7d6881906449
suzuyu0115 <szkyut0115@gmail.com>
2024-04-20 18:13:37 +0900

    050_objects

--- 20_typescript/src/050_objects/start/Example.tsx
+++ 20_typescript/src/050_objects/start/Example.tsx
@@ -1,5 +1,18 @@
 const Example = () => {
   // 練習用
+  const arry1: number[] = [1, 2, 3];
+  const arry2: string[] = ['hello', 'bye'];
+  const arry3: Array<number> = [1, 2, 3];
+  const arry4: (string | number)[] = [1, 2, 3];
+  const arry5: Array<string | number> = [1, 2, 3, 'bye'];
+
+  type Person = { name: string, age?: number }
+  const obj1: Person = { name: 'Taro' };
+
+  const users: Person[] = [
+    {name: 'Taro'},
+    {name: 'Hanako', age: 30}
+  ]
 };
 
 export default Example;
