7eabdc4f6921e6a7e3c8f0c2a8b6dc1497a1e0da
suzuyu0115 <szkyut0115@gmail.com>
2024-04-24 19:58:43 +0900

    130_class

--- 20_typescript/src/130_class/start/Example.tsx
+++ 20_typescript/src/130_class/start/Example.tsx
@@ -1,5 +1,23 @@
+class User {
+  public name: string;
+  private age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
 const Example = () => {
-  // 練習用
+  const user = { name: "Hanako", age: 21 };
+  const user1 = new User("Hanako", 22)
+  console.log(user1);
+  return (
+    <div>
+      <p>Name: {user.name}</p>
+      <p>Age: {user.age}</p>
+    </div>
+  );
 };
 
 export default Example;
