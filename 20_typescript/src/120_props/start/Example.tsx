7472d3962d7b22625f559b2cd15c313bd0b8fac4
suzuyu0115 <szkyut0115@gmail.com>
2024-04-24 19:50:08 +0900

    120_props

--- 20_typescript/src/120_props/start/Example.tsx
+++ 20_typescript/src/120_props/start/Example.tsx
@@ -1,5 +1,13 @@
-const Example = () => {
-  // 練習用
+import Hello, { Btn } from './Hello'
+
+const Example: React.FC = () => {
+    // 練習
+    return(
+        <>
+            <Btn fn={(text) => console.log(`Hello ${text}`)} />
+            <Hello text="TypeScript">Children</Hello>
+        </>
+    )
 };
 
 export default Example;
