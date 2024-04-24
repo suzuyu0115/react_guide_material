0267fdf06fd991979b3e96b906cedcb20f5dc7a1
suzuyu0115 <szkyut0115@gmail.com>
2024-04-24 21:11:54 +0900

    160_add_list_item

---
+++ 20_typescript/src/160_add_list_item/start/TaskList.tsx
@@ -0,0 +1,16 @@
+import { Todo } from './Example';
+
+type TaskListProps = {
+  todos: Todo[]
+}
+
+const TaskList: React.FC<TaskListProps> = ({ todos }) => {
+  return (
+    <ul>
+      {todos.map(todo => {
+        return <li key={todo.id}>{todo.text}</li>
+      })}
+    </ul>
+  )
+}
+export default TaskList;
